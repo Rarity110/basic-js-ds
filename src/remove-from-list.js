f323f75e460f2df23a83ed97649e098584907aff
Rarity110 <rarity110@mail.ru>
2022-05-02 12:27:48 +0300

    remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -15,17 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ * */
+//  class ListNode {
+//     constructor(x) {
+//       this.value = x;
+//       this.next = null;
+//     }
+//  }
+ 
+function removeKFromList(l, k) {
+    let mass = [];
+    while (l) {
+      // let temp = l.pop();
+      if (l.value !== k) {
+      mass.push(l.value);
+      }
+      l = l.next;
+    }
+    return mass.reverse().reduce((previousValue, currentValue) => {
+      console.log(currentValue);
+      if (previousValue === null) {
+        return new ListNode(currentValue);      
+      } else {
+        let newRes = new ListNode(currentValue);
+        newRes.next = previousValue;
+        console.log(newRes);
+        return newRes;
+      }
+    }, null);
 }
+
+// removeKFromList([3, 1, 2, 3, 4, 5], 3)
 
 module.exports = {
   removeKFromList
